c2adb981c88eecc9accd57f1415498feb24a96b8
Jason Wu <jasonwu_8@hotmail.com>
2019-03-20 22:30:31 -0700

    renamed file to something more workable

---
+++ main.py
@@ -0,0 +1,13 @@
+def getOldFiles() -> dict[str, str]:
+    pass
+
+def getNewFiles() -> dict[str, str]:
+    pass
+
+def SymmetricDifference(left : dict[str, str], right : dict[str, str]) -> dict[str, str]:
+    pass
+
+def main():
+    oldFiles = getOldFiles()
+    newFiles = getNewFiles()
+    symmetricDiffFiles = getSymetricDifference(oldFiles, newFiles)
