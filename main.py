05717ab20c6060a2161b54768fd2cac986cd591c
Jason Wu <jasonwu_8@hotmail.com>
2019-03-20 22:48:18 -0700

    wip. main function implemented.

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-def getOldFiles() -> dict[str, str]:
-    pass
+from typing import Dict
 
-def getNewFiles() -> dict[str, str]:
-    pass
+def getOldFiles() -> Dict[str, str]:
+    files = {
+        '1111111111111111111111111111111111111111' : 'file 1.txt',
+        '2222222222222222222222222222222222222222' : 'file 2.txt'
+    }
+    return files
 
-def SymmetricDifference(left : dict[str, str], right : dict[str, str]) -> dict[str, str]:
-    pass
+def getNewFiles() -> Dict[str, str]:
+    files = {
+        '1111111111111111111111111111111111111111' : 'file 4.txt',
+        '7777777777777777777777777777777777777777' : 'file 2.txt',
+        '3333333333333333333333333333333333333333' : 'file 3.txt'
+    }
+    return files
+
+def getSymmetricDifference(left : Dict[str, str], right : Dict[str, str]) -> Dict[str, Dict[str, str]]:
+    for key, value in left.copy().items():
+        if key in right:
+            del right[key]
+            del left[key]
+    return { 'old':left , 'new':right}
 
 def main():
     oldFiles = getOldFiles()
     newFiles = getNewFiles()
-    symmetricDiffFiles = getSymetricDifference(oldFiles, newFiles)
+    symmetricDiffFiles = getSymmetricDifference(oldFiles, newFiles)
+
+    print('old')
+    for oldFiles in symmetricDiffFiles['old']:
+        print(oldFiles)
+
+    print('new')
+    for newFiles in symmetricDiffFiles['new']:
+        print(newFiles)
+
+if __name__ == "__main__":
+    main()
