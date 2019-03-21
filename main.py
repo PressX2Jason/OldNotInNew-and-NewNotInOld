9644b9308b244a203f96250844f0d8f56c7b217c
Jason Wu <jasonwu_8@hotmail.com>
2019-03-21 15:09:25 -0700

    formated file

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 from typing import Dict
 
+
 def getOldFiles() -> Dict[str, str]:
     files = {
-        '1111111111111111111111111111111111111111' : 'file 1.txt',
-        '2222222222222222222222222222222222222222' : 'file 2.txt'
+        '1111111111111111111111111111111111111111': 'file 1.txt',
+        '2222222222222222222222222222222222222222': 'file 2.txt'
     }
     return files
+
 
 def getNewFiles() -> Dict[str, str]:
     files = {
-        '1111111111111111111111111111111111111111' : 'file 4.txt',
-        '7777777777777777777777777777777777777777' : 'file 2.txt',
-        '3333333333333333333333333333333333333333' : 'file 3.txt'
+        '1111111111111111111111111111111111111111': 'file 4.txt',
+        '7777777777777777777777777777777777777777': 'file 2.txt',
+        '3333333333333333333333333333333333333333': 'file 3.txt'
     }
     return files
 
-def getSymmetricDifference(left : Dict[str, str], right : Dict[str, str]) -> Dict[str, Dict[str, str]]:
+
+def getSymmetricDifference(left: Dict[str, str], right: Dict[str, str]) -> Dict[str, Dict[str, str]]:
     for key in left.copy().keys():
         if key in right:
             del right[key]
             del left[key]
-    return { 'old':left , 'new':right}
+    return {'old': left, 'new': right}
+
 
 def main():
     oldFiles = getOldFiles()
@@ -34,6 +38,7 @@ def main():
     print('new')
     for newFiles in symmetricDiffFiles['new']:
         print(newFiles)
+
 
 if __name__ == "__main__":
     main()
