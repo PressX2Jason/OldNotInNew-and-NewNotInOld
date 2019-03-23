2d4a5db14dff73e3baefbf951c8d08963f43527d
Jason Wu <jasonwu_8@hotmail.com>
2019-03-22 19:30:15 -0700

    wip for tests

---
+++ test_main.py
@@ -0,0 +1,23 @@
+import pytest
+import main
+
+
+@pytest.mark.parametrize('', [])
+def test_parserSetup():
+    pass
+
+@pytest.mark.parametrize('', [])
+def test_loggerSetup():
+    pass
+
+@pytest.mark.parametrize('', [])
+def test_getSha1FileNames():
+    pass
+
+@pytest.mark.parametrize('', [])
+def test_writeSha1FileNames():
+    pass
+
+@pytest.mark.parametrize('', [])
+def test_getSymmetricDifference():
+    pass
